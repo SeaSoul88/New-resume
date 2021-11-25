dce64609e629550927d80606c23e3629d22bab05
SeaSoul88 <e.karkavina@gmail.com>
2021-11-25 23:44:15 +0700

    добавила js

---
+++ test.js
@@ -0,0 +1,4 @@
+console.log('test! rocks!');
+
+console.log( "Код JavaScript запустился!", "Привет Лиза!" );
+setTimeout(()=>{document.querySelector(".footer").style["background-color"] = "blue";}, 3000);
