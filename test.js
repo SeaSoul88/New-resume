5d55329a3dbb940c7750d9eecac2d3ebf4d82f38
SeaSoul88 <e.karkavina@gmail.com>
2021-12-01 16:05:12 +0700

    deleting files

--- test.js
+++
@@ -1,4 +0,0 @@
-console.log('test! rocks!');
-
-console.log( "Код JavaScript запустился!", "Привет Лиза!" );
-setTimeout(()=>{document.querySelector(".footer").style["background-color"] = "blue";}, 3000);
