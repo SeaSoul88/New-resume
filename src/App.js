2f6d15289562bc623ab6c9d50921b45c0a784050
SeaSoul88 <e.karkavina@gmail.com>
2021-12-11 12:50:22 +0700

    добавлена страница резюме

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
+import { useState } from 'react'
 import Ava from './components/avatar/Ava.js'
 import First from './components/block/first/first'
 import Second from './components/block/second/second'
+import Page from './components/page/page.js'
 import Footer from './components/footer/footer'
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Link,
+  Route
+} from "react-router-dom";
 
 function App() {
+// const [page, setPage] = useState('auth');
+
   return (
-    <div class="App">
-      <div>
-        <div class="block"></div>
-        <Ava />
-        <header>РЕЗЮМЕ</header>
-        <div class="textcols">
-          <div class="textcols-item">
-            <First /> </div>
+    <BrowserRouter>
+    <div class="App"> 
+    <Link to={"/"}>Авторизация</Link>
+    <br/>
+    <Link to={"/resume"}>Резюме</Link>
+      <Routes>
+        <Route path="/" element={<Page />} />
+        <Route path="resume" element={<div>
+                  <div class="block"></div>
+                  <Ava />
+                  <header>РЕЗЮМЕ</header>
+                  <div class="textcols">
+                    <div class="textcols-item">
+                      <First /> </div>
 
-          <div class="textcols-item">
-            <Second /> </div>
-        </div>
-      </div>
+                    <div class="textcols-item">
+                      <Second /> </div>
+                  </div>
+              </div>} />
+      </Routes> 
       <Footer />
-    </div>
-
-
+      </div>    
+      </BrowserRouter>
   );
 }
 
---
+++ src/components/page/page.js
@@ -0,0 +1,13 @@
+function Page() {
+    return (
+        <>
+            <div>log into squarespace</div>
+            <div>or create account</div>
+            <input type="email" placeholder="email" />
+            <input type="password" placeholder="password" />
+            <button class="test">log in</button>                 
+        </>
+    );
+}
+
+export default Page;
